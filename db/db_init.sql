69f445574d829aca73da59a1d7d7356d61237587
Tanner Zinck <tzinck@uvic.ca>
2017-06-09 23:31:53 -0700

    Finished preliminary version of db creation script

--- db/db_init.sql
+++ db/db_init.sql
@@ -1,25 +1,39 @@
 CREATE TYPE user_status AS ENUM ('active', 'inactive', 'deleted');
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     u_id                serial PRIMARY KEY,
-    first_name          VARHCAR(20),
+    first_name          VARCHAR(20),
     last_name           VARCHAR(20),
-    phone_number        VARVHAR(16),
+    phone_number        VARCHAR(16),
     current_status      user_status          
 );
 
-CREATE TABLE requests (
+CREATE TABLE IF NOT EXISTS incidents (
+    inc_id              serial PRIMARY KEY,
+    requester_imei      VARCHAR(15),
+    req_by_helper       BOOLEAN,
+    time_start          TIMESTAMP WITHOUT TIME ZONE,
+    time_end            TIMESTAMP WITHOUT TIME ZONE
+);
+
+SELECT AddGeometryColumn('incidents', 'init_req_location', 4326, 'POINT', 2);
+
+CREATE TABLE IF NOT EXISTS requests (
     req_id              serial PRIMARY KEY,
+    u_id                INTEGER REFERENCES users(u_id),
+    init_time           TIMESTAMP WITHOUT TIME ZONE,
+    time_reponded       TIMESTAMP WITHOUT TIME ZONE,
+    reponse_val         BOOLEAN,
+    inc_id              INTEGER REFERENCES incidents(inc_id)
+);
+
+--  Adds a 2 dimensional Geometry column of type point, in SRID 4326
+SELECT AddGeometryColumn('requests', 'init_help_location', 4326, 'POINT', 2);
+
+
+CREATE TABLE IF NOT EXISTS location (
     u_id                INTEGER REFERENCES users(u_id)
 );
 
-CREATE TABLE incidents (
-    requester_imei      VARCHAR(15),
-    time_start          TIMESTAMP,
-    time_end            TIMESTAMP
-);
-
-CREATE TABLE location (
-
-);
+SELECT AddGeometryColumn('location', 'help_location', 4326, 'POINT', 2);
 
