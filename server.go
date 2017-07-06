5c6f87d5ea06cececa10992d963436b79f11f105
graemebates <graemebates10@hotmail.com>
2017-07-06 16:56:33 -0700

    updated for heroku, works for local and remote hopefully

--- server.go
+++ server.go
@@ -15,7 +15,7 @@ import (
 // Configuration : Core config structure
 type Configuration struct {
 	Twilio TwilioKey
-	DB DbCreds
+	DB     DbCreds
 }
 
 // TwilioKey : Config strucuture for Twilio
@@ -26,19 +26,21 @@ type TwilioKey struct {
 }
 
 type DbCreds struct {
-	Host string
-	Port int
-	User string
-	Pass string
+	Host   string
+	Port   int
+	User   string
+	Pass   string
 	DbName string
 }
 
 // Globals
 var (
-	done           = make(chan struct{})
-	configuration  = loadConfig()
-	antidoseTwilio = loadTwilio()
-	db             = loadDB()
+	isHeroku       bool   = false
+	heroku         string = os.Getenv("IS_HEROKU")
+	done                  = make(chan struct{})
+	configuration         = loadConfig()
+	antidoseTwilio        = loadTwilio()
+	db                    = loadDB()
 )
 
 func failOnError(err error, msg string) {
@@ -55,24 +57,33 @@ func failGracefully(err error, msg string) {
 }
 
 func loadConfig() Configuration {
-	file, err := os.Open("./config/conf.json")
-	failOnError(err, "Config json not found. Make sure it is present.")
-	decoder := json.NewDecoder(file)
 	configuration := Configuration{}
-	err = decoder.Decode(&configuration)
-	if err != nil {
-		fmt.Println("error:", err)
+	if !isHeroku {
+		file, err := os.Open("./config/conf.json")
+		failOnError(err, "Config json not found. Make sure it is present.")
+		decoder := json.NewDecoder(file)
+
+		err = decoder.Decode(&configuration)
+		if err != nil {
+			fmt.Println("error:", err)
+		}
 	}
 	return configuration
 }
 
 func loadTwilio() *gotwilio.Twilio {
+	if isHeroku {
+		return gotwilio.NewTwilioClient(os.Getenv("TWILIO_SID"), os.Getenv("TWILIO_TOKEN"))
+	}
 	return gotwilio.NewTwilioClient(configuration.Twilio.Sid, configuration.Twilio.Token)
 }
 
 func loadDB() *sql.DB {
-	//psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", configuration.DB.Host, configuration.DB.Port, configuration.DB.User, configuration.DB.Pass, configuration.DB.DbName)
-	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", configuration.DB.Host, configuration.DB.Port, configuration.DB.User, configuration.DB.Pass, configuration.DB.DbName)
+	db, err := sql.Open("postgres", psqlInfo)
+	if isHeroku {
+		db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	}
 	failOnError(err, "Failed to open Postgres")
 
 	err = db.Ping()
@@ -81,6 +92,9 @@ func loadDB() *sql.DB {
 }
 
 func main() {
+	if heroku != "" {
+		isHeroku = true
+	}
 	rand.Seed(time.Now().UTC().UnixNano())
 	initRoutes()
 	<-done
